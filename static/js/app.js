2d6f0cee088db9b5a9ca18488d554d3678c8b7bd
iraismgarcia5 <iraisgarcia05@gmail.com>
2020-01-04 15:27:37 -0500

    comitting final code

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,42 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Function to read each row in the data and append to the table in html
+function ufoReport(data) {
+
+    data.forEach((ufoData) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoData).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+  });
+})
+};
+
+// Filter button
+var button = d3.select("#filter-btn");
+
+button.on("click",function(){
+    // Prevent page refresh
+    d3.event.preventDefault();
+
+    // Select input field
+    var inputElement = d3.select("#datetime");
+
+    // Select input date value
+    var inputValue = inputElement.property("value");
+
+    // Match input with value in dataset
+    var filteredData = tableData.filter(sight => sight.datetime === inputValue);
+    
+    console.log(filteredData);
+
+    tbody.html("");
+    ufoReport(filteredData);
+
+})
+
+ufoReport(tableData);
